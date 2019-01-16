aa8f0d501b1e3fa69569642b935456a79cb2b5fa
Aaron Kim <akim@ameriben.com>
2019-01-16 15:40:52 -0700

    update

--- pythoner.py
+++ pythoner.py
@@ -2,7 +2,7 @@ filename ='C:\Temp\ACTIVEHEALTHYY1.txt'
 filenamenew = filename.split('.')
 with open(filename,'r') as ofileo, open(filenamenew[0]+'1.txt','w') as onewfileo:
     last='dummy'
-    done = 20000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001200000000000000000000000000000000000000
+    done = 'next'
     for line in ofileo:
         dates = line.split('*')
         if dates[1] == '348':
@@ -11,6 +11,6 @@ with open(filename,'r') as ofileo, open(filenamenew[0]+'1.txt','w') as onewfileo
             if dates[3]<last:
                 line.replace(dates[3],last)
         onewfileo.write(line)
-        done =done+1
+        
         print(bin(done))
         
