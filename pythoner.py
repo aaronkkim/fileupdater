166f5453dac6d1cb6c66d462297db9d98a06ee98
Aaron Kim <akim@ameriben.com>
2019-01-17 14:49:41 -0700

    fixing

--- pythoner.py
+++ pythoner.py
@@ -2,15 +2,12 @@ filename ='C:\Temp\ACTIVEHEALTHYY1.txt'
 filenamenew = filename.split('.')
 with open(filename,'r') as ofileo, open(filenamenew[0]+'1.txt','w') as onewfileo:
     last='dummy'
-    done = 'next'
     for line in ofileo:
         dates = line.split('*')
         if dates[1] == '348':
             last = dates[3]
         elif dates[1] =='349':
-            if dates[3]<last:
-                line.replace(dates[3],last)
-        onewfileo.write(line)
-        
-        print(done)
+            if dates[3][:8]<last[:8]:
+               dates[3] = last
+        onewfileo.write('*'.join(dates))
         
